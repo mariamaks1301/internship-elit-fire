da6472d6d8a3d37e56fb764ad818cd8bd4fc6cb8
mariamaks1301 <mariamaks1301@gmail.com>
2023-07-15 17:08:09 +0600

    add sliders

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,30 @@ function ibg(){
 }
     
 ibg();
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+  
     
     
 
